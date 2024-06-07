a6388bfb5db9f99e373400aee3bb201a2112d86d
joao5142 <joaopauloneto3687@gmail.com>
2024-06-07 01:58:17 -0300

    build: adjusting build errors

--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -110,29 +110,3 @@ export const Box = styled.div<IBox>`
       padding-right: ${paddingRight};
     `}
 `;
-
-export const Field = styled.div`
-  height: 42px;
-
-  padding: 0.75rem;
-
-  background: ${({ theme }) => theme.colors.gray_300};
-
-  border: 1px solid ${({ theme }) => theme.colors.gray_400};
-
-  border-radius: 4px;
-
-  color: ${({ theme }) => theme.colors.gray_700};
-
-  font-size: ${({ theme }) => theme.font_size.ROBOTO.s};
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.gray_600};
-  }
-  &:focus {
-    border-color: ${({ theme }) => theme.colors.yellow_900};
-  }
-  &:invalid {
-    border-color: #ff1414b7;
-  }
-`;
